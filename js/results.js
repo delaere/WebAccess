11e4292042aca9dc575a8cfea77ef9c0d9ff8909
Christophe Delaere <christophe.delaere@cern.ch>
2015-09-13 22:15:30 +0200

    Implemented ROOT file navigation
    Still preliminary page, but it does the main things: reads the TFile,
    build a tree representation of directories for navigation, allows to
    browse through these directories, plots TH1F.
    
    Also added ROOT library directly in the project.

--- js/results.js
+++ js/results.js
@@ -1,81 +1,119 @@
-// callback function called to display the ROOT plot
+// callback function called to populate the page from the ROOT file
 
-function createGUI() {
-   // absolute file path can be used as well
-   var filename = "../data/controlPlots_ttbar.root";
-   new JSROOT.TFile(filename, function(file) {
-      file.ReadObject("stage_5/top/bestHmass", function(obj) {
-         JSROOT.draw("drawing", obj, "");
-	 window.objPlot = obj;
-      });
-   });
+// called when ROOT is ready
+function createmyGUI() {
+    // absolute file path can be used as well
+    var filename = "../data/controlPlots_ttbar.root";
+    new JSROOT.TFile(filename, onFileOpen);
+}
+
+// called when the file is open
+function onFileOpen(file) {
+      window.fileObj = file; // for debugging
+      window.fileStructure = parseDirectory(file,file,"",buildFileStructure);
+
+    var activeDir = QueryString.dir;
+    // display the first plot (test)
+    if(activeDir != undefined) {
+      //TODO: get the first TH1F in the dir. (just an example)
+
+    file.ReadDirectory(activeDir,function(dir) {
+        var dirkeys = dir.fKeys;
+        for(var i=0;i<dirkeys.length;i++) {
+            if(dirkeys[i].fClassName==="TH1F") {
+                file.ReadObject(activeDir+"/"+dirkeys[i].fName, function(obj) {
+                    JSROOT.draw("drawing", obj, "");
+                    window.objPlot = obj; // for debugging
+                });
+		break;
+            }
+        }
+    });
+
+    } else {
+    // default
+        //new JSROOT.TFile(filename, function(file) {
+        //   file.ReadObject("stage_5/top/bestHmass", function(obj) {
+        //      JSROOT.draw("drawing", obj, "");
+        //      window.objPlot = obj;
+        //   });
+        //});
+    }
+}
+
+// recursively parse the file to build the directory structure
+var parseDirectoryCount = 0;
+function parseDirectory(dir,file,path, callback) {
+	var keys = dir.fKeys;
+	var structure = [];
+	var lastItem = false;
+	for (i = 0; i < keys.length; i++) {
+		lastItem = (i===keys.length-1);
+		if(keys[i].fClassName=="TDirectory") {
+	                parseDirectoryCount++;
+			file.ReadObject(path+"/"+keys[i].fName, function(obj) {
+				structure.push({ name:obj.dir_name, title:obj.fTitle, content:parseDirectory(obj,file,path+"/"+obj.dir_name,callback) });
+				parseDirectoryCount--;
+				if(parseDirectoryCount===0 && lastItem && callback) callback();
+			});
+		}
+	}
+	return structure;
+}
+
+// once the file has been parsed, build the HTML
+function buildFileStructure() {
+	window.fileStructure.sort(function(a,b){ return a.name>b.name; });
+	$("#fileTree").html($("<li/>").html($("<a/>").attr('href','#').html("ROOT file")));
+	generateHtmlTree($("#fileTree").find("li"),window.fileStructure);
+	$('#fileTree').treed();
+}
+
+// recursive function to convert the structure into HTML
+function generateHtmlTree(rootElement,tree){
+	// create a <ul> and move in there
+	rootElement.append($("<ul/>"));
+	rootElement = rootElement.find("ul");
+	// for each element, create a <li>.
+	for (var i = 0; i < tree.length; i++) {
+		// if there are subdirs, call recursively
+		if(tree[i].content.length>0) {
+			rootElement.append($("<li/>").html(tree[i].title));
+			generateHtmlTree(rootElement.find("li").last(),tree[i].content);
+		} else {
+			rootElement.append($("<li/>").html($("<a/>").attr('href','?dir='+tree[i].name).html(tree[i].title)));
+		}
+	}
 }
 
 // redraw on resize
-
 $(window).resize(function() {
   $("#drawing").html("");
   JSROOT.draw("drawing", window.objPlot, "");
 });
 
-
-// handle the navigation tree (this should probably go in a standalone js)
-
-$.fn.extend({
-    treed: function (o) {
-      
-      var openedClass = 'glyphicon-minus-sign';
-      var closedClass = 'glyphicon-plus-sign';
-      
-      if (typeof o != 'undefined'){
-        if (typeof o.openedClass != 'undefined'){
-        openedClass = o.openedClass;
-        }
-        if (typeof o.closedClass != 'undefined'){
-        closedClass = o.closedClass;
-        }
-      };
-      
-        //initialize each of the top levels
-        var tree = $(this);
-        tree.addClass("tree");
-        tree.find('li').has("ul").each(function () {
-            var branch = $(this); //li with children ul
-            branch.prepend("<i class='indicator glyphicon " + closedClass + "'></i>");
-            branch.addClass('branch');
-            branch.on('click', function (e) {
-                if (this == e.target) {
-                    var icon = $(this).children('i:first');
-                    icon.toggleClass(openedClass + " " + closedClass);
-                    $(this).children().children().toggle();
-                }
-            })
-            branch.children().children().toggle();
-        });
-        //fire event from the dynamically added icon
-      tree.find('.branch .indicator').each(function(){
-        $(this).on('click', function () {
-            $(this).closest('li').click();
-        });
-      });
-        //fire event to open branch if the li contains an anchor instead of text
-        tree.find('.branch>a').each(function () {
-            $(this).on('click', function (e) {
-                $(this).closest('li').click();
-                e.preventDefault();
-            });
-        });
-        //fire event to open branch if the li contains a button instead of text
-        tree.find('.branch>button').each(function () {
-            $(this).on('click', function (e) {
-                $(this).closest('li').click();
-                e.preventDefault();
-            });
-        });
-    }
+$(function(){
 });
 
-//Initialization of treeviews
-
-$('#tree1').treed();
-
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+    return query_string;
+}();
--- pages/results.php
+++ pages/results.php
@@ -41,52 +41,13 @@
 			<h1 class="page-header">SAMADhi results - Browse</h1>
                     </div>
                     <!-- /.col-lg-12 -->
-<!-- menu placeholder for file navigation. To be done in javascript and placed in the sidebar. -->
-        <div class="col-md-4 well ">
-            <ul id="tree1">
-                <li><a href="#">TECH</a>
-                    <ul>
-                        <li>Company Maintenance</li>
-                        <li>Employees
-                            <ul>
-                                <li>Reports
-                                    <ul>
-                                        <li>Report1</li>
-                                        <li>Report2</li>
-                                        <li>Report3</li>
-                                    </ul>
-                                </li>
-                                <li>Employee Maint.</li>
-                            </ul>
-                        </li>
-                        <li>Human Resources</li>
-                    </ul>
-                </li>
-                <li>XRP
-                    <ul>
-                        <li>Company Maintenance</li>
-                        <li>Employees
-                            <ul>
-                                <li>Reports
-                                    <ul>
-                                        <li>Report1</li>
-                                        <li>Report2</li>
-                                        <li>Report3</li>
-                                    </ul>
-                                </li>
-                                <li>Employee Maint.</li>
-                            </ul>
-                        </li>
-                        <li>Human Resources</li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-<!-- end of placeholder -->
+                    <div class="col-md-4 well ">
+                        <ul id="fileTree"></ul>
+                    </div>
                 </div>
                 <!-- /.row -->
                 <div class="row">
-                    <div class="col-lg-12 well" id="drawing" ></div>
+                    <div class="col-lg-4 well" id="drawing" ></div>
                 </div>
                 <!-- /.row -->
             </div>
@@ -98,7 +59,8 @@
     <!-- /#wrapper -->
 
     <?php include "stdScripts.html"; ?>
-    <script src="https://root.cern.ch/js/3.6/scripts/JSRootCore.min.js?io&2d&onload=createGUI" type="text/javascript"></script>
+    <script src="../dist/root/scripts/JSRootCore.js?io&2d&onload=createmyGUI" type="text/javascript"></script>  
+    <script src="../js/treeNavigation.js"></script>
     <script src="../js/results.js"></script>
 
 </body>
