941cf80f89fa4bfe0137100076c35e245a80917f
Christophe Delaere <christophe.delaere@cern.ch>
2015-09-13 13:51:19 +0200

    Plots and menus...
    added a menu (passive for now). Also put the plot in a well, and handle
    resize.

--- pages/results.php
+++ pages/results.php
@@ -41,7 +41,52 @@
 			<h1 class="page-header">SAMADhi results - Browse</h1>
                     </div>
                     <!-- /.col-lg-12 -->
-                    <div class="col-lg-12" id="drawing" ></div>
+<!-- menu placeholder for file navigation. To be done in javascript and placed in the sidebar. -->
+        <div class="col-md-4 well ">
+            <ul id="tree1">
+                <li><a href="#">TECH</a>
+                    <ul>
+                        <li>Company Maintenance</li>
+                        <li>Employees
+                            <ul>
+                                <li>Reports
+                                    <ul>
+                                        <li>Report1</li>
+                                        <li>Report2</li>
+                                        <li>Report3</li>
+                                    </ul>
+                                </li>
+                                <li>Employee Maint.</li>
+                            </ul>
+                        </li>
+                        <li>Human Resources</li>
+                    </ul>
+                </li>
+                <li>XRP
+                    <ul>
+                        <li>Company Maintenance</li>
+                        <li>Employees
+                            <ul>
+                                <li>Reports
+                                    <ul>
+                                        <li>Report1</li>
+                                        <li>Report2</li>
+                                        <li>Report3</li>
+                                    </ul>
+                                </li>
+                                <li>Employee Maint.</li>
+                            </ul>
+                        </li>
+                        <li>Human Resources</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+<!-- end of placeholder -->
+                </div>
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-lg-12 well" id="drawing" ></div>
                 </div>
                 <!-- /.row -->
             </div>
