e93e82a865e95367bfdb6e53d8c37911f88c241f
Christophe Delaere <christophe.delaere@cern.ch>
2015-09-21 00:20:59 +0200

    Introduce the result report page
    Dummy page for now, but the menu is adapted.

---
+++ js/resultReport.js
@@ -0,0 +1,380 @@
+$(function(){
+    //////////////////////////////////////////////////
+    // read the json file to fill in the page content
+    //////////////////////////////////////////////////
+    $.getJSON( "../data/analysisReport.json", function( data  ) {
+        /////////////////////////////////////////////////
+        // samples with missing directory
+        /////////////////////////////////////////////////
+        var missingDirSamples = data["MissingDirSamples"];
+        var missingDirSamples_pg = $("#MissingDirSamples").find(".panel-group");
+        // set header for missing dir samples
+        $("#numberOfMissingDirSamples").html(missingDirSamples.length);
+        if(missingDirSamples.length==0) {
+            $("#MissingDirSamples").addClass("hidden")
+        }
+        // fill missing dir samples
+        for (i = 0; i < missingDirSamples.length; i++) { 
+            missingDirSamples_pg.append(
+                $("<div />").addClass("panel panel-info").append(
+                    $("<div />").addClass("panel-heading").html(
+                        $("<h4 />").addClass("panel-title").html(
+                            $("<a />").attr({"data-toggle":"collapse", "data-parent":"#accordionA", "href":"#collapseA"+i}).html(
+                                missingDirSamples[i]["name"]+" (created on "+missingDirSamples[i]["creation_time"]+" by "+missingDirSamples[i]["author"]+")"
+                            )
+                        )
+                    )
+                ).append(
+                    $("<div />").addClass("panel-collapse collapse").attr({id: "collapseA"+i}).html(
+                        $("<div />").addClass("panel-body").html(
+                            $("<div />").addClass("table-responsive").html(
+                                $("<div />").addClass("table table-hover").html(
+                                    $("<tbody />").html($("<tr />").html("<td>Path</td><td>"+missingDirSamples[i]["path"]+"</td>")).append(
+                                        $("<tr />").html("<td>Type</td><td>"+missingDirSamples[i]["sampletype"]+"</td>")).append(
+                                        $("<tr />").html("<td>number of processed events</td><td>"+missingDirSamples[i]["nevents_processed"]+"</td>")).append(
+                                        $("<tr />").html("<td>number of events</td><td>"+missingDirSamples[i]["nevents"]+"</td>")).append(
+                                        $("<tr />").html("<td>(Effective) luminosity</td><td>"+missingDirSamples[i]["luminosity"]+"</td>")).append(
+                                        $("<tr />").html("<td>Normalization</td><td>"+missingDirSamples[i]["normalization"]+"</td>")).append(
+                                        $("<tr />").html("<td>Code version</td><td>"+missingDirSamples[i]["code_version"]+"</td>")).append(
+                                        $("<tr />").html("<td>Comment</td><td>"+missingDirSamples[i]["user_comment"]+"</td>")).append(
+                                        $("<tr />").html("<td>Source dataset</td><td>"+missingDirSamples[i]["source_dataset_id"]+"</td>")).append(
+                                        $("<tr />").html("<td>Source sample</td><td>"+missingDirSamples[i]["source_sample_id"]+"</td>")
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+
+        }
+        /////////////////////////////////////////////////
+        // database inconsistencies
+        /////////////////////////////////////////////////
+        var dbProblems = data["DatabaseInconsistencies"];
+        var dbProblems_pg = $("#DbProblems").find(".panel-group");
+        // set header for db inconsistencies
+        $("#numberOfDbProblems").html(dbProblems.length);
+        if(dbProblems.length==0) {
+            $("#DbProblems").addClass("hidden");
+        }
+        // fill db inconsistencies
+        for (i = 0; i < dbProblems.length; i++) {
+            dbProblems_pg.append(
+                $("<div />").addClass("panel panel-info").append(
+                    $("<div />").addClass("panel-heading").html(
+                        $("<h4 />").addClass("panel-title").html(
+                            $("<a />").attr({"data-toggle":"collapse", "data-parent":"#accordionB", "href":"#collapseB"+i}).html(
+                                dbProblems[i][0]["name"]+" (created on "+dbProblems[i][0]["creation_time"]+" by "+dbProblems[i][0]["author"]+")  "
+                            )
+                        ).append(
+                            $("<span />").addClass("label label-danger").html( 
+                                $("<strong />").html(dbProblems[i][1])
+                            )
+                        )
+                    )
+                ).append(
+                    $("<div />").addClass("panel-collapse collapse").attr({id: "collapseB"+i}).html(
+                        $("<div />").addClass("panel-body").html(
+                            $("<div />").addClass("table-responsive").html(
+                                $("<div />").addClass("table table-hover").html(
+                                    $("<tbody />").html($("<tr />").html("<td>Path</td><td>"+dbProblems[i][0]["path"]+"</td>")).append(
+                                        $("<tr />").html("<td>Type</td><td>"+dbProblems[i][0]["sampletype"]+"</td>")).append(
+                                        $("<tr />").html("<td>number of processed events</td><td>"+dbProblems[i][0]["nevents_processed"]+"</td>")).append(
+                                        $("<tr />").html("<td>number of events</td><td>"+dbProblems[i][0]["nevents"]+"</td>")).append(
+                                        $("<tr />").html("<td>(Effective) luminosity</td><td>"+dbProblems[i][0]["luminosity"]+"</td>")).append(
+                                        $("<tr />").html("<td>Normalization</td><td>"+dbProblems[i][0]["normalization"]+"</td>")).append(
+                                        $("<tr />").html("<td>Code version</td><td>"+dbProblems[i][0]["code_version"]+"</td>")).append(
+                                        $("<tr />").html("<td>Comment</td><td>"+dbProblems[i][0]["user_comment"]+"</td>")).append(
+                                        $("<tr />").html("<td>Source dataset</td><td>"+dbProblems[i][0]["source_dataset_id"]+"</td>")).append(
+                                        $("<tr />").html("<td>Source sample</td><td>"+dbProblems[i][0]["source_sample_id"]+"</td>")
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        }
+        /////////////////////////////////////////////////
+        // Plots with db statistics
+        /////////////////////////////////////////////////
+        var statistics = data["SampleStatistics"];
+        // Number of events in samples
+        var sampleNevents = statistics["sampleNevents"];
+        $('#sampleNeventsContainer').highcharts({
+            series: [{
+                name: "Samples",
+                data: sampleNevents
+            }],
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: ''
+            },
+            legend: {
+                enabled: false
+            },
+            xAxis: { 
+                title: {
+                    text:'Number of Events',
+                    align: 'high'
+                }
+            },
+            yAxis: { 
+                type: 'logarithmic' ,
+                title: {
+                    text:'Number of Samples',
+                    align: 'high'
+                }
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0,
+                    borderWidth: 0,
+                    groupPadding: 0,
+                    shadow: false
+                }
+            }
+        });
+        // Evolution of the number of events in samples
+        var sampleNeventsTimeprof = statistics["sampleNeventsTimeprof"];
+        $('#sampleNeventsTimeprofContainer').highcharts({
+            chart: {
+                type: 'spline'
+            },
+            title: {
+                text: ''
+            },
+            legend: {
+                enabled: false
+            },
+            xAxis: { 
+                type: 'datetime',
+                title: {
+                    text:'Date',
+                    align: 'high'
+                }
+            },
+            yAxis: { 
+                type: 'logarithmic' ,
+                title: {
+                    text:'Number of Events',
+                    align: 'high'
+                }
+            },
+            plotOptions: {
+                spline: {
+                    marker: {
+                        enabled: true
+                    }
+                }
+            },
+            series: [{
+                name: "SAMADhi",
+                data: sampleNeventsTimeprof
+            }]
+        });
+        // Number of events processed
+        var sampleNeventsProcessed = statistics["sampleNeventsProcessed"];
+        $('#sampleNeventsProcessedContainer').highcharts({
+            series: [{
+                name: "SAMADhi",
+                data: sampleNeventsProcessed
+            }],
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: ''
+            },
+            legend: {
+                enabled: false
+            },
+            xAxis: { 
+                title: {
+                    text:'Number of Events',
+                    align: 'high'
+                }
+            },
+            yAxis: { 
+                type: 'logarithmic' ,
+                title: {
+                    text:'Number of Samples',
+                    align: 'high'
+                }
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0,
+                    borderWidth: 0,
+                    groupPadding: 0,
+                    shadow: false
+                }
+            }
+        });
+        // Evolution of the number of events processed
+        var sampleNeventsProcessedTimeprof = statistics["sampleNeventsProcessedTimeprof"];
+        $('#sampleNeventsProcessedTimeprofContainer').highcharts({
+            chart: {
+                type: 'spline'
+            },
+            title: {
+                text: ''
+            },
+            legend: {
+                enabled: false
+            },
+            xAxis: { 
+                type: 'datetime',
+                title: {
+                    text:'Date',
+                    align: 'high'
+                }
+            },
+            yAxis: { 
+                type: 'logarithmic' ,
+                title: {
+                    text:'Number of Events',
+                    align: 'high'
+                }
+            },
+            plotOptions: {
+                spline: {
+                    marker: {
+                        enabled: true
+                    }
+                }
+            },
+            series: [{
+                name: "SAMADhi",
+                data: sampleNeventsProcessedTimeprof
+            }]
+        });
+        // Time profile
+        var samplesTimeprof = statistics["samplesTimeprof"];
+        $('#timeProfileContainer').highcharts({
+            chart: {
+                type: 'spline'
+            },
+            title: {
+                text: ''
+            },
+            legend: {
+                enabled: false
+            },
+            xAxis: { 
+                type: 'datetime',
+                title: {
+                    text:'Date',
+                    align: 'high'
+                }
+            },
+            yAxis: { 
+                type: 'logarithmic' ,
+                title: {
+                    text:'Number of Samples',
+                    align: 'high'
+                }
+            },
+            plotOptions: {
+                spline: {
+                    marker: {
+                        enabled: true
+                    }
+                }
+            },
+            series: [{
+                name: "SAMADhi",
+                data: samplesTimeprof
+            }]
+        });
+        // Sample Types
+        var sampleTypes = statistics["sampleTypes"];
+            $('#typePlotContainer').highcharts({
+                chart: {
+                    type: 'pie',
+                    options3d: {
+                        enabled: true,
+                        alpha: 45,
+                        beta: 0
+                    }
+                },
+                title: {
+                    text: ''
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        size: '85%',
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        depth: 35,
+                        dataLabels: {
+                            enabled: true,
+                            format: '{point.name}'
+                        }
+                    }
+                },
+                series: [{
+                    type: 'pie',
+                    name: 'Samples share',
+            data: sampleTypes
+                }]
+            });
+        $("#typePlotContainer").siblings().filter(".panel-heading").html("Samples type")
+        // Samples Author
+        var sampleAuthors = statistics["sampleAuthors"];
+        $('#authorsPlotContainer').highcharts({
+            chart: {
+                type: 'pie',
+                options3d: {
+                    enabled: true,
+                    alpha: 45,
+                    beta: 0
+                }
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    size: '85%',
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    depth: 35,
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.name}'
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Samples share',
+                data: sampleAuthors
+            }]
+        });
+        $("#authorsPlotContainer").siblings().filter(".panel-heading").html("Samples author")
+    } );
+    // radial gradiant
+    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
+        return {
+            radialGradient: {
+                cx: 0.5,
+                cy: 0.3,
+                r: 0.7
+            },
+            stops: [
+                [0, color],
+                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+            ]
+        };
+    });
+});
---
+++ pages/resultsReport.php
@@ -0,0 +1,154 @@
+<?php
+    require("config.php");
+    if(empty($_SESSION['user']))
+    {
+        header("Location: login.php");
+        die("Redirecting to login.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Christophe Delaere">
+
+    <title>Nemrod's Dashboard</title>
+
+    <?php include "stdHeaders.html"; ?>
+
+    <!-- Custom CSS specialization-->
+    <link href="../dist/css/webAccess.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <?php include "navigation.html"; ?>
+
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+			<h1 class="page-header">SAMADhi results - Database analysis report</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                Time profile
+                            </div>
+                            <div class="panel-body" id="timeProfileContainer">
+                            </div>
+                        </div>
+		    </div>
+                    <div class="col-lg-6 col-xs-12">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                Samples Author
+                            </div>
+                            <div class="panel-body" id="authorsPlotContainer">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xs-12">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                Sample Types
+                            </div>
+                            <div class="panel-body" id="typePlotContainer">
+                            </div>
+                        </div>
+                    </div>
+            </div><div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-primary" id="MissingDirSamples">
+                        <div class="panel-heading">
+                            Samples with missing path <span class="badge" id="numberOfMissingDirSamples"></span>
+                        </div>
+                        <!-- .panel-heading -->
+                        <div class="panel-body">
+                            <div class="panel-group" id="accordionA">
+				<p>The following samples are pointing to non-existant locations on the server.</p>
+                            </div>
+                        </div>
+                        <!-- .panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+		    <div class="col-lg-12 col-xs-12">
+                        <div class="panel panel-primary" id="DbProblems">
+                            <div class="panel-heading">
+				Database Inconsistencies <span class="badge" id="numberOfDbProblems"></span>
+                            </div>
+                            <div class="panel-body">
+                                <div class="panel-group" id="accordionB">
+			            <p>The following samples have inconsitencies in their relation to other entries.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xs-12">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                Evolution of the number of events in samples
+                            </div>
+                            <div class="panel-body" id="sampleNeventsTimeprofContainer">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xs-12">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                Number of events in samples
+                            </div>
+                            <div class="panel-body" id="sampleNeventsContainer">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xs-12">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                Evolution of the number of events processed
+                            </div>
+                            <div class="panel-body" id="sampleNeventsProcessedTimeprofContainer">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xs-12">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                Number of events processed
+                            </div>
+                            <div class="panel-body" id="sampleNeventsProcessedContainer">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+
+    <?php include "stdScripts.html"; ?>
+
+    <script src="../js/resultReport.js"></script>
+
+</body>
+
+</html>
