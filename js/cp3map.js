78e2d00e146602e392324fe0ac99d91a88205d6f
Christophe Delaere <christophe.delaere@cern.ch>
2015-12-22 23:50:24 +0100

    New demo page with a map of CP3 floors

---
+++ js/cp3map.js
@@ -0,0 +1,208 @@
+$(function () { 
+    // load building layouts
+    $('#cp3level1').css("min-height",$('#cp3level1').width());
+    $('#cp3level1').svg({loadURL: 'CP3level1.svg', onLoad: mapCallback});
+    $('#cp3level1').css("min-height",20);
+    var svg1 = $('#cp3level1').svg('get');
+    $('#cp3level2').css("min-height",$('#cp3level2').width())
+    $('#cp3level2').svg({loadURL: 'CP3level2.svg', onLoad: mapCallback});
+    $('#cp3level2').css("min-height",20);
+    var svg2 = $('#cp3level2').svg('get');
+    $('#cp3level3').css("min-height",$('#cp3level3').width())
+    $('#cp3level3').svg({loadURL: 'CP3level3.svg', onLoad: mapCallback});
+    $('#cp3level3').css("min-height",20);
+    var svg3 = $('#cp3level3').svg('get');
+    //populate the form (combo box and list for search
+    $.getJSON( "../data/towerE.json","salt="+makeid(), prepareForm );
+    //register handlers for the form
+    $('#sel1').on('change',function(){
+           // handler for the event
+           console.log($("#sel1").val());
+           highlightRoom($("#sel1").val());
+    });
+    $('#search1 .btn').click(function(){
+           // handler for search click
+           console.log($('#search1 .form-control').val());
+           var room = findRoom($('#search1 .form-control').val());
+           highlightRoom(room);
+    });
+    $("#search1 .btn").mouseup(function(){
+       $(this).blur();
+    });
+    // disable the submit when pressing enter in the input field
+    $('#mapform').on('keyup keypress', function(e) {
+      var code = e.keyCode || e.which;
+      if (code == 13) { 
+        e.preventDefault();
+        return false;
+      }
+    });
+
+});
+
+function findRoom(name) {
+    for (var idx=0; idx<allnames.length; idx++) {
+       var n = allnames[idx].toLowerCase().search(name.toLowerCase());
+       if (n!=-1) {
+          return associatedRooms[idx];
+       }
+    }
+}
+
+function highlightRoom(room) {
+
+    if (typeof lastHighlight != 'undefined')
+      lastHighlight.css("fill","white");
+    var re = /(\w)\.(\d{3})/;
+    var area = '#$1$2_area';
+    var purpose = '#$1$2_purpose';
+    var purpose2 = '#$1$2_purpose2';
+    var user1 = '#$1$2_user1';
+    var user2 = '#$1$2_user2';
+    var user3 = '#$1$2_user3';
+    lastHighlight = $(room.replace(re, area));
+    lastHighlight.css("fill","yellow");
+
+    // scroll there
+    $('html, body').animate({
+        scrollTop: lastHighlight.parents("svg").offset().top
+    }, 2000);
+
+    // modal popup with some text from one room
+    $("#myModal .modal-title").html(room);
+    var body = $("#myModal .modal-body p");
+    body.html("");
+    body.append($("<p/>").html($(room.replace(re, purpose)).html()),
+                $("<p/>").html($(room.replace(re, purpose2)).html()),
+                $("<p/>").html($(room.replace(re, user1)).html()),
+                $("<p/>").html($(room.replace(re, user2)).html()),
+                $("<p/>").html($(room.replace(re, user3)).html())
+    );
+    $('#myModal').modal('show');
+
+}
+
+function prepareForm(data) {
+    // Fill Combobox
+    for (var floor in data.floors) {
+    	if(data.floors.hasOwnProperty(floor)) {
+    		for(var room in data.floors[floor]) {
+    			$("#sel1").append($("<option/>").html(data.floors[floor][room]));
+    		}
+    	}
+    }
+    // Build a list of names that we can search for
+    allnames = [];
+    associatedRooms = []
+    var excludedNames=["","NA","None","XX","AN"];
+    for (var room in data.rooms) {
+    	if(data.rooms.hasOwnProperty(room)) {
+		if(!isInArray(data.rooms[room].user1,excludedNames)) {
+			allnames.push(data.rooms[room].user1);
+			associatedRooms.push(room);
+		}
+		if(!isInArray(data.rooms[room].user2,excludedNames)) {
+			allnames.push(data.rooms[room].user2);
+			associatedRooms.push(room);
+		}
+		if(!isInArray(data.rooms[room].user3,excludedNames)) {
+			allnames.push(data.rooms[room].user3);
+			associatedRooms.push(room);
+		}
+    	}
+    }
+}
+
+function isInArray(value, array) {
+  return array.indexOf(value) > -1;
+}
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function mapCallback() {
+    // auto-resize
+    var svg = $(this).children("svg")
+    svg.width("100%");
+    svg.height("100%");
+
+    // load the SVG
+    var floor = $(this).attr("data-floor");
+    $.getJSON( "../data/towerE.json","salt="+makeid(), (function(thisfloor, thissvg) { return function(data) { fillmap(data,thisfloor,thissvg); };  }(floor,svg)) );
+
+}
+
+function fillmap(data, floor, svg) {
+    var floor = data.floors[floor];
+    var rooms = data.rooms;
+    var re = /(\w)\.(\d{3})/; 
+    var entity = '#$1$2_entity'; 
+    var purpose = '#$1$2_purpose';
+    var purpose2 = '#$1$2_purpose2';
+    var user1 = '#$1$2_user1'; 
+    var user2 = '#$1$2_user2'; 
+    var user3 = '#$1$2_user3'; 
+    //console.log(floor);
+    
+    for(var roomid=0;roomid<floor.length;roomid++) {
+      var roomName = floor[roomid];
+      var room = rooms[floor[roomid]];
+
+      // user NA means not available
+      // user None means empty
+      // in both cases, display nothing
+      if (room.user1=="NA") { room.user1="" }
+      if (room.user2=="NA") { room.user2="" }
+      if (room.user3=="NA") { room.user3="" }
+      if (room.user1=="None") { room.user1="" }
+      if (room.user2=="None") { room.user2="" }
+      if (room.user3=="None") { room.user3="" }
+      $(svg).find(roomName.replace(re, entity)).html(room.owner);
+      var purposes = room.purpose.split("\n");
+      if(purposes.length==1) { purposes.push(""); }
+
+      // check if there is a purpose in the svg and in the json
+      // depending on the case, do the best we can to render properly
+      // purposes and users.
+      var hasPurpose = $(svg).find(roomName.replace(re, purpose)).length;
+      var hasPurpose2 = $(svg).find(roomName.replace(re, purpose2)).length;
+      if(hasPurpose && hasPurpose2) {
+          $(svg).find(roomName.replace(re, purpose)).html(purposes[0]);
+          $(svg).find(roomName.replace(re, purpose2)).html(purposes[1]);
+          $(svg).find(roomName.replace(re, user1)).html(room.user1);
+          $(svg).find(roomName.replace(re, user2)).html(room.user2);
+          $(svg).find(roomName.replace(re, user3)).html(room.user3);
+	      
+      } else if (hasPurpose) {
+          $(svg).find(roomName.replace(re, purpose)).html(room.purpose);
+          $(svg).find(roomName.replace(re, user1)).html(room.user1);
+          $(svg).find(roomName.replace(re, user2)).html(room.user2);
+          $(svg).find(roomName.replace(re, user3)).html(room.user3);
+      } else {
+	      if(purposes[1]!="") {
+		      room.user3=room.user1;
+		      room.user1=purposes[0];
+		      room.user2=purposes[1];
+	      } else if(purposes[0]!="") {
+		      room.user3=room.user2;
+		      room.user2=room.user1;
+		      room.user1=purposes[0];
+	      }
+              $(svg).find(roomName.replace(re, user1)).html(room.user1);
+              $(svg).find(roomName.replace(re, user2)).html(room.user2);
+              $(svg).find(roomName.replace(re, user3)).html(room.user3);
+      }
+    }
+}
+
+
+// another issue is how to handle the form/the parameters on the address line. 
+// for the demo, write another page with the room or user as argument. 
---
+++ pages/cp3map.php
@@ -0,0 +1,115 @@
+<?php
+    require("config.php");
+    if(empty($_SESSION['user']))
+    {
+        header("Location: login.php");
+        die("Redirecting to login.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Christophe Delaere">
+
+    <title>Nemrod's Dashboard</title>
+
+    <?php include "stdHeaders.html"; ?>
+    <link rel="stylesheet" type="text/css" href="../bower_components/jquery-svg/jquery.svg.css"> 
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <?php include "navigation.html"; ?>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" role="dialog">
+          <div class="modal-dialog">
+          
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Modal Header</h4>
+              </div>
+              <div class="modal-body">
+                <p>Some text in the modal.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                  <!-- Form -->
+                  <div class="col-lg-6">
+                    <div class="panel panel-default">
+                      <div class="panel-heading">
+                         Search and Selection
+                      </div>
+                      <div class="panel-body">
+                        <div class="row">
+                          <div class="col-lg-12">
+                            <form role="form" id="mapform">
+                              <div class="form-group">
+                                <label for="sel1">Room:</label>
+                                <select class="form-control" id="sel1" name="room">
+                                </select>
+                              </div>
+                              <label for="search1">Search Person:</label>
+                              <div class="input-group custom-search-form" id="search1">
+                                <input type="text" class="form-control" placeholder="Search..." name="search">
+                                <span class="input-group-btn">
+                                  <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                  </button>
+                                </span>
+                              </div>
+                            </form>
+                          </div> <!-- col-lg-12 -->
+                        </div> <!-- row -->
+                      </div> <!-- panel-body -->
+                    </div> <!-- panel -->
+                  </div> <!-- col-lg-6 -->
+                  <!-- Content -->
+                  <div class="col-lg-12">
+		      <h1 class="page-header">Level 1</h1>
+		  </div>
+                  <div class="col-lg-12" id="cp3level1" data-floor="floor1">
+		  </div>
+                  <div class="col-lg-12">
+		      <h1 class="page-header">Level 2</h1>
+                  </div>
+                  <div class="col-lg-12" id="cp3level2" data-floor="floor2">
+		  </div>
+                  <div class="col-lg-12">
+		      <h1 class="page-header">Level 3</h1>
+                  </div>
+                  <div class="col-lg-12" id="cp3level3" data-floor="floor3">
+		  </div>
+                </div>
+            </div>  <!-- /.container-fluid -->
+        </div>  <!-- /#page-wrapper -->
+    </div>  <!-- /#wrapper -->
+
+    <?php include "stdScripts.html"; ?>
+    <script type="text/javascript" src="../bower_components/jquery-svg/jquery.svg.js"></script>
+    <script src="../js/cp3map.js"></script>
+
+
+</body>
+
+</html>
